3ab69ace7d871ea85b107c4810918351d3a0bba6
uzairsaqib786 <uzair.saqib@enterprise64.com>
2023-11-14 14:38:50 +0500

    [PLM-5862] | [LogixPro | Manual Fixations | medium + low] | [Flowrack Replenishment]

---
+++ src/app/common/constants/strings.constants.ts
@@ -0,0 +1,90 @@
+export const StringAssignments = {
+    WorkstationNotAssignedToZone: 'This workstation is not assigned to a zone'
+};
+
+export const ResponseStrings = {
+    No: 'No',
+    Empty: '',
+    Null: null
+}
+
+export const ToasterType = {
+    Error: 'error',
+    Success: 'success'
+}
+
+export const ToasterTitle = {
+    Error: 'Error!',
+    Success: 'Success!'
+}
+
+export const ToasterMessages = {
+    NoOpenLocations: 'There are no open locations.',
+    NoLocationForItem: 'No Locations found for Item Number, Scan or Select an open Location.',
+    ItemNotInInventory: 'This item does not exist in Inventory Master for this carton flow zone.',
+    LocationUnavailable: 'Location Unavailable.',
+    QuantityMustGreaterZero: 'Quantity must be greater than zero.',
+    EnterQuantity: 'Please enter a quantity.',
+    ItemQuantityAdded: 'Item Quantity Added',
+    QuantityErrorInventoryMap: 'The quantity was not entered due to an error in the Inventory Map'
+}
+
+export const AppNames = {
+    ICSAdmin: 'ICSAdmin',
+    ConsolidationManager: 'Consolidation Manager',
+    Induction: 'Induction',
+    FlowRackReplenish: 'FlowRackReplenish',
+    ImportExport: 'ImportExport',
+    Markout: 'Markout',
+    OrderManager: 'OrderManager',
+    WorkManager: 'WorkManager'
+}
+
+export const AppRoutes = {
+    Admin: '/admin',
+    ConsolidationManager: '/ConsolidationManager',
+    InductionManager: '/InductionManager',
+    FlowrackReplenish: '/FlowrackReplenish',
+    Hash: '#',
+    OrderManager: '/OrderManager',
+}
+
+export const AppIcons = {
+    ManageAccounts: 'manage_accounts',
+    InsertChart: 'insert_chart',
+    CheckList: 'checklist',
+    Schema: 'schema',
+    ElectricBolt: 'electric_bolt',
+    PendingActions: 'pending_actions',
+    FactCheck: 'fact_check'
+}
+
+export const RouteNames = {
+    Admin: 'Admin',
+    ConsolidationManager: 'Consolidation Manager',
+    InductionManager: 'Induction Manager',
+    FlowRackReplenishment: 'FlowRack Replenishment',
+    ImportExport: 'Import Export',
+    Markout: 'Markout',
+    OrderManager: 'Order Manager',
+    WorkManager: 'Work Manager'
+}
+
+export const RouteUpdateMenu = {
+    Admin: 'admin',
+    Consolidation: 'consolidation',
+    Induction: 'induction',
+    Empty: '',
+    OrderManager: 'orderManager'
+}
+
+export const AppPermissions = {
+    AdminMenu: 'Admin Menu',
+    ConsolidationManager: 'Consolidation Manager',
+    InductionManager: 'Induction Manager',
+    FlowRackReplenish: 'FlowRack Replenish',
+    ImportExport: 'Import Export',
+    Markout: 'Markout',
+    OrderManager: 'Order Manager',
+    WorkManager: 'Work Manager'
+}
