e607a5d54452b35204f1e6e91b246ea25440b5fa
Shan E64 <shan.asghar@enterprise64.com>
2023-11-29 10:10:26 +0500

    [PLM-5862] | [LogixPro | Manual Fixations | Medium And Low Items] | [Intercepter bug fixed]
    [PLM-5862] | [LogixPro | Manual Fixations | Medium And Low Items] | [Intercepter bug fixed]

--- src/app/common/init/header-interceptor.interceptor.ts
+++ src/app/common/init/header-interceptor.interceptor.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { catchError, Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -25,7 +19,7 @@ export class HeaderInterceptor implements HttpInterceptor {
   public iUserApi : IUserAPIService;
 
   constructor(
-	  public userApi : UserApiService,
+	  public userApi: UserApiService,
     private router: Router,
     private global: GlobalService,
     private dialog:MatDialog,
@@ -46,48 +40,43 @@ export class HeaderInterceptor implements HttpInterceptor {
     );
   }
 
-  private handleAuthError(err: HttpErrorResponse): Observable<any> { 
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
     if (err.status === 401) {
       if(this.router.url.split('?')[0] != '/report-view'){
-        if(this.authService.isConfigUser()){
-          this.iGlobalConfigApi.configLogout().subscribe((res:any) => {
+        if(this.authService.isConfigUser())
+          this.iGlobalConfigApi.configLogout().subscribe((res: any) => {
             if (res.isExecuted) {       
               this.dialog.closeAll();
               this.global.ShowToastr(ToasterType.Error, 'Token Expire', ToasterTitle.Error);
               window.location.href = "/#/globalconfig"; 
-            } else {
-              this.global.ShowToastr(ToasterType.Error, res.responseMessage, ToasterTitle.Error);
-              console.log("configLogout",res.responseMessage);
-            }
+            } else this.global.ShowToastr(ToasterType.Error, res.responseMessage, ToasterTitle.Error);
           });    
-        } else {
-          this.iUserApi.Logout().subscribe((res:any) => {
-            if (res.isExecuted) {  
-              let lastRoute: any = localStorage.getItem('LastRoute') ? localStorage.getItem('LastRoute') : ""; 
+        else
+          this.iUserApi.Logout().subscribe((res: any) => {
+            if (res.isExecuted) {
+              let lastRoute: any = localStorage.getItem('LastRoute') ? localStorage.getItem('LastRoute') : "";
               if(lastRoute != "") localStorage.setItem('LastRoute', lastRoute);
-              if(!localStorage.getItem('LastRoute')) localStorage.setItem('LastRoute', this.router.url);     
+              if(!localStorage.getItem('LastRoute')) localStorage.setItem('LastRoute', this.router.url);
               this.dialog.closeAll();
-              this.global.ShowToastr(ToasterType.Error,'Token Expire', ToasterTitle.Error);  
-              if((this.router.url.indexOf('login') <= -1)) localStorage.setItem('LastRoute', this.router.url);        
-              this.router.navigate(['/login']);    
-            } else {
-              this.global.ShowToastr(ToasterType.Error, res.responseMessage, ToasterTitle.Error);
-              console.log("Logout",res.responseMessage);
-            }
-          })
-        }
+              this.global.ShowToastr(ToasterType.Error,'Token Expire', ToasterTitle.Error);
+              if((this.router.url.indexOf('login') <= -1)) localStorage.setItem('LastRoute', this.router.url);
+              this.router.navigate(['/login']);
+            } else this.global.ShowToastr(ToasterType.Error, res.responseMessage, ToasterTitle.Error);
+          });
 
         return of(err.message);
       }
       throw err;
     } else if(err.status === 500) {
-      if(`${err.url}`.indexOf("insertnewprinter") > -1) this.global.ShowToastr(ToasterType.Error, err.error.ResponseMessage, ToasterTitle.Error);  
+      if(`${err.url}`.indexOf("insertnewprinter") > -1) this.global.ShowToastr(ToasterType.Error, err.error.ResponseMessage, ToasterTitle.Error);
       this.spinnerService.hide();
     } else if(err.status === 403) {
-      this.global.ShowToastr(ToasterType.Error, `Unauthorize access ${err.error.ResponseMessage ? `(${err.error.ResponseMessage})` : ''}`, ToasterTitle.Error);
-      this.router.navigate(['/#/dashboard']);
+      const responseMessage = err.error?.ResponseMessage ? `(${err.error.ResponseMessage})` : '';
+      const errorMessage = 'Unauthorize access ' + responseMessage;
+      this.global.ShowToastr(ToasterType.Error, errorMessage, ToasterTitle.Error);
+      this.router.navigate(['/dashboard']);
     }
 
     return of(err.message);
-  } 
+  }
 }
