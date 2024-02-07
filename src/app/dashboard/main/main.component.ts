3ac021a9105cf9ceb8f2ce88bf16ce5f54ea8239
Hadi Bhojani <abdul.hadi@enterprise64.com>
2024-02-07 12:16:41 +0500

    [PLST-238] | [bulk-pick-screen-functionality] | [bulk process - bulk pick - temp menu added + corrections]

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -65,11 +65,12 @@ export class MainComponent implements OnInit {
         if (res?.data) {
           // temp BulkProcess
           res.data.wsAllAppPermission.push("BulkProcess");
-          res.data.appLicenses.BulkProcess = res.data.appLicenses.FlowRackReplenish;
+          res.data.appLicenses.BulkProcess = JSON.parse(JSON.stringify(res.data.appLicenses.FlowRackReplenish));
           res.data.appLicenses.BulkProcess.info.displayName = "BulkProcess";
           res.data.appLicenses.BulkProcess.info.name = "BulkProcess";
           res.data.appLicenses.BulkProcess.info.url = "BulkProcess";
           // temp BulkProcess
+          
           this.convertToObj(res.data);
           localStorage.setItem('availableApps', JSON.stringify(this.applicationData));
           this.sharedService.setMenuData(this.applicationData);
