e31093ad50daebf777396cc1899e36695260b331
Hadi Bhojani <abdul.hadi@enterprise64.com>
2024-02-22 18:07:12 +0500

    [PLST-237] | [Admin Menu – Preferences – Bulk Zones] | [workstation Preferences implementation - [Validate Tote IDs],[Maintain Bulk Pick Batch ID] added]

--- src/app/admin/admin-preferences/workstation-preferences/wp-workstation-setup/wp-ws-tote-management/wp-ws-tote-management.component.ts
+++ src/app/admin/admin-preferences/workstation-preferences/wp-workstation-setup/wp-ws-tote-management/wp-ws-tote-management.component.ts
@@ -39,6 +39,8 @@ export class WpWsToteManagementComponent implements OnInit {
         this.companyObj.carManifest = res.data.pfSettingsII.filter((x: any) => x.pfName == "Carousel Manifest")[0].pfSetting == 1 ? true : false;
         this.companyObj.offCarManifest = res.data.pfSettingsII.filter((x: any) => x.pfName == "Off Carousel Manifest")[0].pfSetting == 1 ? true : false;
         this.companyObj.batchHotPut = res.data.pfSettingsII.filter((x: any) => x.pfName == "Batch Hot Put Away")[0].pfSetting;
+        this.companyObj.ValidateToteIDs = res.data.validateToteIDs;
+        this.companyObj.MaintainBulkPickBatchID = res.data.maintainBulkPickBatchID;
       }
       else {
         this.global.ShowToastr(ToasterType.Error, this.global.globalErrorMsg(), ToasterTitle.Error);
@@ -56,7 +58,9 @@ export class WpWsToteManagementComponent implements OnInit {
       "batchHotPut": this.companyObj.batchHotPut,
       "carManifest": this.companyObj.carManifest,
       "offCarManifest": this.companyObj.offCarManifest,
-      "autoToteManifest": this.companyObj.autoToteManifest
+      "autoToteManifest": this.companyObj.autoToteManifest,
+      "ValidateToteIDs": this.companyObj.ValidateToteIDs,
+      "MaintainBulkPickBatchID": this.companyObj.MaintainBulkPickBatchID
     }
     this.saveForm(payload);
   }
