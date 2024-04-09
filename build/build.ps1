62c23176a48ccbff4b90e3cd8f095d5fcf93fc0c
Nick <nick.hoefer@peaklogix.com>
2024-04-08 23:07:19 -0400

    improving speed of build

--- build/build.ps1
+++ build/build.ps1
@@ -28,7 +28,7 @@ Set-Location $projectDirectory
 write-host "Building project in $projectDirectory"
 
 write-host "Running npm install"
-npm install
+npm ci
 
 write-host "Deleting dist directory"
 Remove-Item -Recurse -Force "dist"
