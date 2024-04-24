b5ac7851d810aa0a719469372a296fc245bf4691
Andrew Jackson <andrew.jackson@peaklogix.com>
2024-04-24 09:24:56 -0400

    [PLST-2153] - Pick to Tote - Fixed issue with auto generated tote ID Fixed issue with totes IDs not incrementing correctly when assigning more than one order

--- src/app/admin/dialogs/bm-toteid-entry/bm-toteid-entry.component.ts
+++ src/app/admin/dialogs/bm-toteid-entry/bm-toteid-entry.component.ts
@@ -72,13 +72,14 @@ export class BmToteidEntryComponent implements OnInit {
   }
 
   createNextTote() {
-    if (this.view != 'batch' && this.view != 'tote') {
-      this.selectedList.forEach((element, i) => {
-        this.bulkProcessApiService.BatchNextTote(this.selectedList.length).then((res) => {
-          this.nextToteID = res.body?.nextId;
+    if(this.view != 'batch' && this.view != 'tote'){
+      this.bulkProcessApiService.BatchNextTote(this.selectedList.length).then((res) => {
+        this.nextToteID = res.body?.nextId;
+        this.selectedList.forEach((element, i) => {
           this.selectedList[i].IsTote = false;
           this.selectedList[i].IsError = false;
-          this.selectedList[i]['toteId'] = parseInt(this.nextToteID);
+          this.selectedList[i]['toteId'] =
+            parseInt(this.nextToteID) + i + 1;
         });
       });
     }
