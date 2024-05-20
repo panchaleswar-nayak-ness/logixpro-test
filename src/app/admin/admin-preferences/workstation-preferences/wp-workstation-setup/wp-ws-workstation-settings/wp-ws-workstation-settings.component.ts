6b574c16f170786a8bb60ff3465fed097226ef15
NabeelKhan <nabeel.khan@enterprise64.com>
2024-05-20 16:04:33 +0500

    [PLST-2321] | [LogixPro - Can't set default printer to a valid printer]

--- src/app/admin/admin-preferences/workstation-preferences/wp-workstation-setup/wp-ws-workstation-settings/wp-ws-workstation-settings.component.ts
+++ src/app/admin/admin-preferences/workstation-preferences/wp-workstation-setup/wp-ws-workstation-settings/wp-ws-workstation-settings.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from 'src/app/common/init/auth.service';
 import { IAdminApiService } from 'src/app/common/services/admin-api/admin-api-interface';
 import { AdminApiService } from 'src/app/common/services/admin-api/admin-api.service';
 import { GlobalService } from 'src/app/common/services/global.service';
+import { IGlobalConfigApi } from 'src/app/common/services/globalConfig-api/global-config-api-interface';
+import { GlobalConfigApiService } from 'src/app/common/services/globalConfig-api/global-config-api.service';
 
 @Component({
   selector: 'app-wp-ws-workstation-settings',
@@ -11,27 +13,33 @@ import { GlobalService } from 'src/app/common/services/global.service';
   styleUrls: ['./wp-ws-workstation-settings.component.scss']
 })
 export class WpWsWorkstationSettingsComponent implements OnInit {
-
+  listReportPrinter:any;
+  listLabelPrinter:any;
   public iAdminApiService: IAdminApiService;
   userData: any;
   companyObj: any = {};
-
+  public  iGlobalConfigApi: IGlobalConfigApi;
   constructor(
     public authService: AuthService, 
     private global: GlobalService, 
+    public globalConfigApi: GlobalConfigApiService,
     public adminApiService: AdminApiService) {
-    this.iAdminApiService = adminApiService;
+    this.iAdminApiService = adminApiService; 
+    this.iGlobalConfigApi = globalConfigApi;
     this.userData = authService.userData();
   }
 
   ngOnInit(): void {
     this.companyInfo();
+    this.getAllPrinters();
   }
   
   public companyInfo() {
     this.iAdminApiService.WorkstationSetupInfo().subscribe((res: any) => {
       if (res.isExecuted && res.data) {
         this.companyObj.scanPicks = res.data.scanVerifyPicks;
+        this.companyObj.printLabelLocation = res.data.printLabelLocation;
+        this.companyObj.printReportLocation = res.data.printReportLocation;
         this.companyObj.scanCounts = res.data.scanVerifyCounts;
         this.companyObj.scanPuts = res.data.scanVerifyPutAways;
         this.companyObj.quickPick = res.data.pfSettings.filter((x:any) => x.pfName == "Quick Pick")[0].pfSetting == 1 ? true : false;
@@ -51,14 +59,28 @@ export class WpWsWorkstationSettingsComponent implements OnInit {
       "scanPicks": this.companyObj.scanPicks,
       "scanCounts": this.companyObj.scanCounts,
       "scanPuts": this.companyObj.scanPuts,
-      "printRepLocation": "",
-      "printLabelLocation": "",
+      "printRepLocation": this.companyObj.printReportLocation,
+      "printLabelLocation":this.companyObj.printLabelLocation,
       "quickPick": this.companyObj.quickPick,
       "defQuickPick": this.companyObj.defQuickPick,
     }
     this.saveForm(payload);
   }
 
+  getAllPrinters(){
+    this.iGlobalConfigApi.GetAllPrinters().subscribe((res:any)=>{
+      if(res)
+      {
+        this.listLabelPrinter = res.data.filter(x=>x.label == "Able to Print Labels");
+        this.listReportPrinter = res.data.filter(x=>x.label == "Not Able to Print Labels");
+      }
+      else {
+        this.global.ShowToastr(ToasterType.Error, this.global.globalErrorMsg(), ToasterTitle.Error);
+        console.log("GetAllPrinters",res.responseMessage);
+      } 
+        
+    });
+  }
   async saveForm(paylaod) {
     this.iAdminApiService.WorkstationSettingsUpdate(paylaod).subscribe((res: any) => {
     })
