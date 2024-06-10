255ce095776ad3ccd5b76eb9284c6f32ae52b394
Hadi Bhojani <abdul.hadi@enterprise64.com>
2024-06-10 12:26:58 +0500

    [PLST-2376] | [LogixPro - Markout | Frontend Development ] | [Side Menu Fix]

--- src/app/markout/markout.component.ts
+++ src/app/markout/markout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IMarkoutApiService } from '../common/services/markout-api/markout-api-interface';
 import { MarkoutApiService } from '../common/services/markout-api/markout-api-service';
 import { ToteDataResponse } from './models/markout-model';
+import { SharedService } from '../common/services/shared.service';
 
 @Component({
   selector: 'app-markout',
@@ -13,11 +14,14 @@ export class MarkoutComponent implements OnInit {
   toteDataResponse: ToteDataResponse;
   toteId: string;
 
-  constructor(public markoutApiService: MarkoutApiService) {
+  constructor(public markoutApiService: MarkoutApiService,private sharedService: SharedService,) {
     this.iMarkoutApiService = markoutApiService;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let appd=JSON.parse(localStorage.getItem('availableApps') ?? '');
+    this.sharedService.setMenuData(appd);
+  }
 
   getToteData() {
     this.iMarkoutApiService
