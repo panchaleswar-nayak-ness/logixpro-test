dadb8302c3c9c468244a6e41640073faaa4bedf7
huzaifaasiff42101 <145774798+arbashhumayun-e64@users.noreply.github.com>
2024-09-26 17:13:16 +0500

    [PLST-3671] [Movado - Inventory Map Delete Not Working][This context for the api service is lost due to  js callback function]

--- src/app/admin/dialogs/delete-confirmation/delete-confirmation.component.ts
+++ src/app/admin/dialogs/delete-confirmation/delete-confirmation.component.ts
@@ -13,7 +13,7 @@ import { GlobalConfigApiService } from 'src/app/common/services/globalConfig-api
 import { ICommonApi } from 'src/app/common/services/common-api/common-api-interface';
 import { CommonApiService } from 'src/app/common/services/common-api/common-api.service';
 import { GlobalService } from 'src/app/common/services/global.service';
-import { Mode, ToasterTitle, ToasterType ,ResponseStrings} from 'src/app/common/constants/strings.constants';
+import { Mode, ToasterTitle, ToasterType, ResponseStrings } from 'src/app/common/constants/strings.constants';
 import { UserSession } from 'src/app/common/types/CommonTypes';
 
 @Component({
@@ -25,16 +25,16 @@ export class DeleteConfirmationComponent implements OnInit {
   action = 'remove';
   actionMessage = '';
   Message: any;
-  public userData : UserSession;
+  public userData: UserSession;
 
   public iAdminApiService: IAdminApiService;
-  public iConsolidationAPI : IConsolidationApi;
-  public iCommonAPI : ICommonApi;
+  public iConsolidationAPI: IConsolidationApi;
+  public iCommonAPI: ICommonApi;
   public iGlobalConfigApi: IGlobalConfigApi;
 
   constructor(
-    public consolidationAPI : ConsolidationApiService,
-    public commonAPI : CommonApiService,
+    public consolidationAPI: ConsolidationApiService,
+    public commonAPI: CommonApiService,
     private dialog: MatDialog,
     public globalConfigApi: GlobalConfigApiService,
     private global: GlobalService,
@@ -43,9 +43,9 @@ export class DeleteConfirmationComponent implements OnInit {
     private authService: AuthService,
     private router: Router,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { 
-    this.iConsolidationAPI = consolidationAPI; 
-    this.iAdminApiService = adminApiService; 
+  ) {
+    this.iConsolidationAPI = consolidationAPI;
+    this.iAdminApiService = adminApiService;
     this.iGlobalConfigApi = globalConfigApi;
     this.iCommonAPI = commonAPI;
   }
@@ -63,111 +63,111 @@ export class DeleteConfirmationComponent implements OnInit {
       return;
     }
 
-    switch (this.data.mode) {
-      case Mode.DeleteZone:
-        this.handleDelete(this.adminApiService.deleteEmployeeZone, {
+    // Mapping the API calls based on the mode
+    const modeToApiMap = {
+      [Mode.DeleteZone]: {
+        method: () => this.adminApiService.deleteEmployeeZone({
           zone: this.data.zone,
           username: this.data.userName,
-        });
-        break;
-      case Mode.DeletePickLevel:
-        this.iAdminApiService.deletePickLevels({
+        })
+      },
+      [Mode.DeletePickLevel]: {
+        method: () => this.iAdminApiService.deletePickLevels({
           levelID: this.data.picklevel.levelID.toString(),
           startShelf: this.data.picklevel.startCarousel.toString(),
           endShelf: this.data.picklevel.endCarousel.toString(),
           userName: this.data.userName,
           wsid: this.userData.wsid
-        }).subscribe(
-          (res: any) => {
-            if (res.isExecuted) {
-              this.global.ShowToastr(ToasterType.Success, labels.alert.delete, ToasterTitle.Success);
-              this.dialogRef.close({ isExecuted: true });
-            } else {
-              this.global.ShowToastr(ToasterType.Error, labels.alert.went_worng, ToasterTitle.Error);
-              this.dialogRef.close({ isExecuted: false });
-            }
-          },
-          (error: any) => {
-            this.global.ShowToastr(ToasterType.Error, labels.alert.went_worng, ToasterTitle.Error);
-          }
-        );
-        break;
-      case Mode.DeleteLocation:
-        this.handleDelete(this.adminApiService.deleteEmployeeLocation, {
+        })
+      },
+      [Mode.DeleteLocation]: {
+        method: () => this.adminApiService.deleteEmployeeLocation({
           startLocation: this.data.location.startLocation,
           endLocation: this.data.location.endLocation,
           username: this.data.userName,
-        });
-        break;
-      case Mode.DeleteConnectionString:
-        this.handleDelete(this.globalConfigApi.ConnectionDelete, {
+        })
+      },
+      [Mode.DeleteConnectionString]: {
+        method: () => this.globalConfigApi.ConnectionDelete({
           ConnectionName: this.data.connectionName,
-        });
-        break;
-      case Mode.DeleteGroup:
-      case Mode.DeleteAllowedGroup:
-        this.handleDelete(this.adminApiService.deleteGroup, {
+        })
+      },
+      [Mode.DeleteGroup]: {
+        method: () => this.adminApiService.deleteGroup({
           GroupName: this.data.grp_data?.groupName || this.data.allowedGroup,
           userName: this.data.userName,
-        });
-        break;
-      case Mode.DeleteAllowedFunction:
-        this.handleDelete(this.adminApiService.deleteControlName, {
+        })
+      },
+      [Mode.DeleteAllowedGroup]: {
+        method: () => this.adminApiService.deleteGroup({
+          GroupName: this.data.grp_data?.groupName || this.data.allowedGroup,
+          userName: this.data.userName,
+        })
+      },
+      [Mode.DeleteAllowedFunction]: {
+        method: () => this.adminApiService.deleteControlName({
           controlName: this.data.controlName,
           userName: this.data.userName,
-        });
-        break;
-      case Mode.DeleteInvMap:
-        this.handleDelete(this.adminApiService.deleteInventoryMap, {
-          inventoryMapID: this.data.id,
-        });
-        break;
-      case Mode.DeleteEmp:
-        this.handleDelete(this.adminApiService.deleteAdminEmployee, {
+        })
+      },
+      [Mode.DeleteInvMap]: {
+        method: () => this.adminApiService.deleteInventoryMap({
+          inventoryMapID: this.data.id
+        })
+      },
+      [Mode.DeleteEmp]: {
+        method: () => this.adminApiService.deleteAdminEmployee({
           userName: this.data.emp_data.userName,
           deleteBy: this.userData.userName,
-        });
-        break;
-      case Mode.DeleteGrpAllowed:
-        this.handleDelete(this.adminApiService.deleteUserGroup, {
+        })
+      },
+      [Mode.DeleteGrpAllowed]: {
+        method: () => this.adminApiService.deleteUserGroup({
           groupname: this.data.allowedGroup.groupName,
           username: this.data.allowedGroup.userName,
-        });
-        break;
-      case Mode.DeleteWarehouse:
-        this.handleDelete(this.commonAPI.dltWareHouse, {
+        })
+      },
+      [Mode.DeleteWarehouse]: {
+        method: () => this.commonAPI.dltWareHouse({
           warehouse: this.data.warehouse,
-        });
-        break;
-      case Mode.DeleteVelocity:
-        this.handleDelete(this.commonAPI.dltVelocityCode, {
+        })
+      },
+      [Mode.DeleteVelocity]: {
+        method: () => this.commonAPI.dltVelocityCode({
           velocity: this.data.velocity,
-        });
-        break;
-      case Mode.DeleteOrderStatus:
-        this.handleDelete(this.adminApiService.DeleteOrderStatus, this.data.paylaod);
-        break;
-      case Mode.DeleteCarrier:
-        this.handleDelete(this.consolidationAPI.CarrierDelete, {
+        })
+      },
+      [Mode.DeleteOrderStatus]: {
+        method: () => this.adminApiService.DeleteOrderStatus(this.data.payload)
+      },
+      [Mode.DeleteCarrier]: {
+        method: () => this.consolidationAPI.CarrierDelete({
           carrier: this.data.carrier,
-        });
-        break;
-      case Mode.DeleteWorkstation:
-        this.handleDelete(this.globalConfigApi.WorkStationDelete);
-        break;
-      case Mode.DeleteCategory:
-        this.handleDelete(this.commonAPI.CategoryDelete, {
+        })
+      },
+      [Mode.DeleteWorkstation]: {
+        method: () => this.globalConfigApi.WorkStationDelete()
+      },
+      [Mode.DeleteCategory]: {
+        method: () => this.commonAPI.CategoryDelete({
           Category: this.data.category,
           SubCategory: this.data.subCategory,
-        });
-        break;
-      default:
-        this.dialogRef.close(ResponseStrings.Yes);
+        })
+      }
+    };
+
+    // Execute the method based on the mode, if it exists in the map
+    const selectedAction = modeToApiMap[this.data.mode];
+    if (selectedAction) {
+      this.handleDelete(selectedAction.method);
+    } else {
+      this.dialogRef.close(ResponseStrings.Yes);
     }
   }
 
-  private handleDelete(apiMethod: Function, payload?: any) {
-    apiMethod(payload).subscribe(
+  // Centralized method to handle API calls
+  private handleDelete(apiMethod: () => any) {
+    apiMethod().subscribe(
       (res: any) => {
         if (res.isExecuted) {
           this.global.ShowToastr(ToasterType.Success, labels.alert.delete, ToasterTitle.Success);
@@ -191,8 +191,8 @@ export class DeleteConfirmationComponent implements OnInit {
 
   reloadCurrentRoute() {
     let currentUrl = this.router.url;
-    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => { 
-      this.router.navigate([currentUrl]); 
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
     });
   }
 }
