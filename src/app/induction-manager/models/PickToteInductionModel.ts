3a3673aea69295847a830ea5338adcc8f3197cb8
fahim.akhtar <fahim.akhtar@enterprise64.com>
2024-10-04 16:31:42 +0500

    PLST-2749 || Frontend - Adding "Pick Tote Induction Filters" Modal and Modify Functionality ||  worked on main screen pick tote filter popup.
    PLST-2749 || Frontend - Adding "Pick Tote Induction Filters" Modal and Modify Functionality ||  worked on main screen pick tote filter popup.

--- src/app/induction-manager/models/PickToteInductionModel.ts
+++ src/app/induction-manager/models/PickToteInductionModel.ts
@@ -1,16 +1,16 @@
 export class AddPickToteInductionFilter {
-    id: number;
-    alias?: string;
-    ppField?: string;
-    startCharacter: number;
-    endCharacter: number;
+  id: number;
+  alias?: string;
+  ppField?: string;
+  startCharacter: number;
+  endCharacter: number;
 }
 
 export class PickToteInductionFilter {
-    id: number;
-    alias?: string;
-    ppField?: string;
-    startCharacter: number;
-    endCharacter: number;
+  id: number;
+  alias?: string;
+  ppField?: string;
+  startCharacter: number;
+  endCharacter: number;
+  Value?: string;
 }
-
--- src/app/induction-manager/pick-tote-induction/pick-tote-in-filter/pick-tote-in-filter.component.ts
+++ src/app/induction-manager/pick-tote-induction/pick-tote-in-filter/pick-tote-in-filter.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { GlobalService } from 'src/app/common/services/global.service';
+import { IInductionManagerApiService } from 'src/app/common/services/induction-manager-api/induction-manager-api-interface';
+import { InductionManagerApiService } from 'src/app/common/services/induction-manager-api/induction-manager-api.service';
+import { PickToteInductionFilter } from '../../models/PickToteInductionModel';
 
 @Component({
   selector: 'app-pick-tote-in-filter',
@@ -8,17 +12,102 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class PickToteInFilterComponent implements OnInit {
 
-  constructor() { }
+  public iInductionManagerApi: IInductionManagerApiService;
+  filters: PickToteInductionFilter[] = [];
+  apiFilterData: PickToteInductionFilter[] = [];
+
+  constructor(
+    public inductionManagerApi: InductionManagerApiService,
+    private global: GlobalService
+
+  ) {
+    this.iInductionManagerApi = inductionManagerApi;
+  }
+
+  aliasFilterList =[
+    { colHeader: '', colDef: ''}
+  ];
 
   displayedColumns: string[] = ["select", "field", "fieldValue", "actions"];
   elementData = [
-    { field: 'Zone 1'},
-    { field: 'Zone 2'},
-    { field: 'Zone 3' }
+    { field: 'Zone 1'}
   ];
-  dataSource = new MatTableDataSource(this.elementData);
 
   ngOnInit(): void {
+    this.GetPickToteInductionFilterData();
+    this.initializeRows(); // Initialize table rows independently of the API response
   }
 
+// Initialize table with empty rows, not based on API response
+initializeRows() {
+  const initialRows: PickToteInductionFilter[] = [
+    { id: 0, alias: '', ppField: '', startCharacter: 0, endCharacter: 0, Value: "" }
+  ];
+  this.filters = initialRows;
+}
+
+// Method to add a new row
+addRow() {
+  const newRow: PickToteInductionFilter = {
+    id: 0, // Temporary ID; will be set from API data
+    alias: '', // Will be set based on ppField selection
+    ppField: '', // User selects this
+    startCharacter: 0, // Will be set from API data
+    endCharacter: 0, // Will be set from API data
+    Value: '' // User inputs this
+  };
+  this.filters =[newRow,...this.filters];
+}
+
+  // Method to remove a row
+  removeRow(index: number) {
+    if(this.filters.length  > 1 ){
+      this.filters.splice(index, 1); // Remove row from the filters array
+      this.filters = [...this.filters]; // Trigger change detection
+    }
+  }
+
+  GetPickToteInductionFilterData() {
+    this.iInductionManagerApi.GetPickToteInductionFilter().subscribe((res: any) => {
+      if (res.data && res.isExecuted) {
+        // Store the complete API data
+        this.apiFilterData = res.data;
+  
+        // Populate aliasFilterList with alias values from API
+        this.aliasFilterList = this.apiFilterData.map((item: any) => ({
+          colHeader: item.alias,
+          colDef: item.alias,
+        }));
+      }
+    });
+  }
+  selectionChange(value: any) {
+    console.log('Selected value:', value);
+  }
+  applyFilter() {
+    console.log("selected data : " + this.filters);
+    const finalFilters = this.filters.map(filter => {
+      // Find the corresponding API data based on ppField selection
+      const apiData = this.apiFilterData.find(api => api.alias === filter.ppField);
+      if (apiData) {
+        return {
+          id: apiData.id,
+          alias: apiData.alias,
+          ppField: apiData.ppField,
+          startCharacter: apiData.startCharacter,
+          endCharacter: apiData.endCharacter,
+          Value: filter.Value
+        };
+      } else {
+        // Handle the case where ppField does not match any API data
+        console.log(`No API data found for ppField: ${filter.ppField}`);
+        return null;
+      }
+    }).filter(filter => filter !== null); // Remove any null entries
+  
+    console.log('Final Filters:', finalFilters);
+  
+    // You can now send `finalFilters` to your backend API or use it as needed
+  }
+  
 }
