b4a5c2a5bd255555e88e6a6d725220d93bd20994
danish.farman <danish.farman@enterprise64.com>
2024-11-06 16:55:09 +0500

    [PLST-4024]|[added handling for no data found message on table after filteration]

--- src/app/induction-manager/pick-tote-induction/non-super-batch-orders/non-super-batch-orders.component.ts
+++ src/app/induction-manager/pick-tote-induction/non-super-batch-orders/non-super-batch-orders.component.ts
@@ -29,7 +29,7 @@ import { MatInput } from '@angular/material/input';
 import { InductionManagerApiService } from 'src/app/common/services/induction-manager-api/induction-manager-api.service';
 import { IInductionManagerApiService } from 'src/app/common/services/induction-manager-api/induction-manager-api-interface';
 import { catchError, Observable, throwError } from 'rxjs';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AuthService } from 'src/app/common/init/auth.service';
@@ -58,7 +58,7 @@ export class NonSuperBatchOrdersComponent implements OnInit, AfterViewInit {
   @Input() set specificInductionSuccess(event: EventEmitter<void>) {
     event.subscribe(() => this.focusFirstInput());
   }
-  @Input() transactionQtyRecieved:any
+  @Input() transactionQtyRecieved: any;
   userData;
   elementData = [
     {
@@ -97,13 +97,27 @@ export class NonSuperBatchOrdersComponent implements OnInit, AfterViewInit {
     'toteScanned',
   ];
 
+  customPagination: any = {
+    total: '',
+    recordsPerPage: 10,
+    startIndex: 0,
+    endIndex: 10,
+  };
+
   public iInductionManagerApi: IInductionManagerApiService;
   filters: PickToteInductionFilter[] = [];
   orderNumberFilter: string = '';
   dataSource: MatTableDataSource<any>;
   toteScanned: any;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.customPagination = {
+      total: '',
+      recordsPerPage: 10,
+      startIndex: 0,
+      endIndex: 10,
+    };
+  }
 
   ngAfterViewInit(): void {
     this.updatedPaginator();
@@ -114,10 +128,17 @@ export class NonSuperBatchOrdersComponent implements OnInit, AfterViewInit {
     this.dataSource = new MatTableDataSource(data);
     this.updatedPaginator();
     this.updateSorting();
-    if(this.transactionQtyRecieved == 0){
+
+    if (this.transactionQtyRecieved == 0) {
       this.focusFirstInput();
     }
+  }
 
+  handlePageEvent(e: PageEvent) {
+    this.customPagination.startIndex = e.pageSize * e.pageIndex;
+    this.customPagination.endIndex = e.pageSize * e.pageIndex + e.pageSize;
+    this.customPagination.recordsPerPage = e.pageSize;
+    this.rebind();
   }
 
   updatedPaginator() {
--- src/app/induction-manager/pick-tote-induction/super-batch-orders/super-batch-orders.component.ts
+++ src/app/induction-manager/pick-tote-induction/super-batch-orders/super-batch-orders.component.ts
@@ -24,7 +24,7 @@ import { InductionManagerApiService } from 'src/app/common/services/induction-ma
 import { IInductionManagerApiService } from 'src/app/common/services/induction-manager-api/induction-manager-api-interface';
 import { catchError, Observable, throwError } from 'rxjs';
 import { MatSort, Sort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { MatInput } from '@angular/material/input';
 import { AuthService } from 'src/app/common/init/auth.service';
@@ -50,16 +50,7 @@ export class SuperBatchOrdersComponent implements OnInit, AfterViewInit {
   @ViewChild('paginator') paginator: MatPaginator;
   @ViewChildren(MatInput) toteInputs!: QueryList<MatInput>;
   userData;
-  displayedColumns: string[] = [
-    'itemNumber',
-    // 'zone',
-    'priority',
-    'quality',
-    'requiredDate',
-    'totalOrderQty',
-    'toteScanned',
-  ];
-
+  
   elementData = [
     {
       itemNumber: 'Zone 1',
@@ -84,13 +75,37 @@ export class SuperBatchOrdersComponent implements OnInit, AfterViewInit {
     },
   ];
 
+  displayedColumns: string[] = [
+    'itemNumber',
+    // 'zone',
+    'priority',
+    'quality',
+    'requiredDate',
+    'totalOrderQty',
+    'toteScanned',
+  ];
+
+  customPagination: any = {
+    total: '',
+    recordsPerPage: 10,
+    startIndex: 0,
+    endIndex: 10,
+  };
+
   public iInductionManagerApi: IInductionManagerApiService;
   filters: PickToteInductionFilter[] = [];
   orderNumberFilter: string = '';
   dataSource: MatTableDataSource<any>;
   toteScanned: any;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.customPagination = {
+      total: '',
+      recordsPerPage: 10,
+      startIndex: 0,
+      endIndex: 10,
+    };
+  }
 
   ngAfterViewInit(): void {
     this.updatedPaginator();
@@ -124,6 +139,13 @@ export class SuperBatchOrdersComponent implements OnInit, AfterViewInit {
     this.focusFirstInput();
   }
 
+  handlePageEvent(e: PageEvent) {
+    this.customPagination.startIndex = e.pageSize * e.pageIndex;
+    this.customPagination.endIndex = e.pageSize * e.pageIndex + e.pageSize;
+    this.customPagination.recordsPerPage = e.pageSize;
+    this.rebind();
+  }
+  
   updatedPaginator() {
     if (this.dataSource && this.dataSource.filteredData.length > 0)
       this.dataSource.paginator = this.paginator;
