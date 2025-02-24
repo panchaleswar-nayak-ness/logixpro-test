f39b94b975c323ae5722a2e744cf1388c10491a5
Abdullah-Haroon <abdullah.haroon@enterprise64.com>
2025-02-24 18:24:49 +0500

    PLST-5627 | Movado | Highlight Markout Screen Fields

--- src/app/consolidation-manager/cm-markout/markout-status/markout-status.component.ts
+++ src/app/consolidation-manager/cm-markout/markout-status/markout-status.component.ts
@@ -4,7 +4,7 @@ import { CmPreferences } from 'src/app/common/types/CommonTypes';
 
 class info {
   title: string;
-  value: string;
+  value: number;
   colorClass: string;
 }
 
@@ -15,12 +15,12 @@ class info {
 })
 export class MarkoutStatusComponent implements OnInit {
   info: info[] = [
-    { title: 'Current Status', value: '-', colorClass: 'label-gray' },
-    { title: 'Missed', value: '0', colorClass: 'Reprocess-card' },
-    { title: 'Short', value: '0', colorClass: 'Open-card' },
-    { title: 'Ship Short', value: '0', colorClass: 'label-blue2' },
-    { title: 'Complete', value: '0', colorClass: 'Compete-cart' },
-    { title: 'Not Inducted', value: '0', colorClass: 'not-inducted-status' }
+    { title: 'Current Status', value: 0, colorClass: 'label-gray' },
+    { title: 'Missed', value: 0, colorClass: 'Reprocess-card' },
+    { title: 'Short', value: 0, colorClass: 'Open-card' },
+    { title: 'Ship Short', value: 0, colorClass: 'label-blue2' },
+    { title: 'Complete', value: 0, colorClass: 'Compete-cart' },
+    { title: 'Not Inducted', value: 0, colorClass: 'not-inducted-status' }
   ];
 
   @Input() toteDataResponse: ToteDataResponse;
@@ -37,41 +37,47 @@ export class MarkoutStatusComponent implements OnInit {
       changes['toteDataResponse'] &&
       changes['toteDataResponse']['currentValue']
     ) {
-      this.info[0].value = this.toteDataResponse.toteStatus == "" ? "-" : this.toteDataResponse.toteStatus;
-      this.info[3].value = this.toteDataResponse.data
-        .filter((element) => element.status == 'Ship Short')
-        .length.toString();
-      this.info[2].value = this.toteDataResponse.data
-        .filter((element) => element.status == 'Short')
-        .length.toString();
-      this.info[4].value = this.toteDataResponse.data
-        .filter((element) => element.status == 'Complete')
-        .length.toString();
-      this.info[1].value = this.toteDataResponse.data
-        .filter((element) => element.status == 'Missed')
-        .length.toString();
-      this.info[5].value = this.toteDataResponse.data
-        .filter((element) => element.status == 'Not Inducted')
-        .length.toString();
+     
+  this.info[0].value = this.toteDataResponse.toteStatus === "" ? 0 : Number(this.toteDataResponse.toteStatus);
+
+  this.info[3].value = this.toteDataResponse.data
+    .filter((element) => element.status == 'Ship Short')
+    .length;
+
+  this.info[2].value = this.toteDataResponse.data
+    .filter((element) => element.status == 'Short')
+    .length; 
+
+  this.info[4].value = this.toteDataResponse.data
+    .filter((element) => element.status == 'Complete')
+    .length; 
+
+  this.info[1].value = this.toteDataResponse.data
+    .filter((element) => element.status == 'Missed')
+    .length;
+
+  this.info[5].value = this.toteDataResponse.data
+    .filter((element) => element.status == 'Not Inducted')
+    .length; 
+
       this.isBlossomed = this.toteDataResponse.blossomCount > 0;
     }
 
-
       let elements: NodeListOf<Element>[] = [];
     
-      if (this.markoutPreference?.missed === true) {
+      if (this.markoutPreference?.missed === true && this.info[1]?.value > 0) {  
         elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.Reprocess-card'));
       }
-      if (this.markoutPreference?.short === true) {
+      if (this.markoutPreference?.short === true && this.info[2]?.value > 0) {
         elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.Open-card'));
       }
-      if (this.markoutPreference?.complete === true) {
+      if (this.markoutPreference?.complete === true && this.info[4]?.value > 0) {
         elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.Compete-cart'));
       }
-      if (this.markoutPreference?.notIncluded === true) {
+      if (this.markoutPreference?.notIncluded === true && this.info[5]?.value > 0) {
         elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.not-inducted-status'));
       }
-      if (this.markoutPreference?.shipShort === true) {
+      if (this.markoutPreference?.shipShort === true && this.info[3]?.value > 0) {
         elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.label-blue2'));
       }
     
@@ -82,8 +88,5 @@ export class MarkoutStatusComponent implements OnInit {
         });
       });
  
-    console.log(this.markoutPreference.missed);
-    console.log(this.markoutPreference.short);
-    console.log(this.markoutPreference.shipShort);
   }
 }
