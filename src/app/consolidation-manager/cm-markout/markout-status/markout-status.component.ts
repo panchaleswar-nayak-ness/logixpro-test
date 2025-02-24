8c0c6c196c275394b1a8bd4ffbdb07bacf700625
Abdullah-Haroon <abdullah.haroon@enterprise64.com>
2025-02-24 15:00:11 +0500

    PLST-5627 |  Movado | Highlight Markout Screen Fields

--- src/app/consolidation-manager/cm-markout/markout-status/markout-status.component.ts
+++ src/app/consolidation-manager/cm-markout/markout-status/markout-status.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, ElementRef, Renderer2 } from '@angular/core';
 import { ToteDataResponse } from '../models/markout-model';
+import { CmPreferences } from 'src/app/common/types/CommonTypes';
 
 class info {
   title: string;
   value: string;
   colorClass: string;
 }
+
 @Component({
   selector: 'app-markout-status',
   templateUrl: './markout-status.component.html',
@@ -22,35 +24,66 @@ export class MarkoutStatusComponent implements OnInit {
   ];
 
   @Input() toteDataResponse: ToteDataResponse;
-  isBlossomed : boolean = false;
+  @Input() markoutPreference: CmPreferences;
 
-  constructor() {}
+  isBlossomed: boolean = false;
 
-  ngOnInit(): void {
-  }
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (
       changes['toteDataResponse'] &&
       changes['toteDataResponse']['currentValue']
     ) {
-      this.info[0].value = this.toteDataResponse.toteStatus == ""? "-" : this.toteDataResponse.toteStatus; 
+      this.info[0].value = this.toteDataResponse.toteStatus == "" ? "-" : this.toteDataResponse.toteStatus;
       this.info[3].value = this.toteDataResponse.data
-        .filter((element) => (element.status == 'Ship Short'))
+        .filter((element) => element.status == 'Ship Short')
         .length.toString();
       this.info[2].value = this.toteDataResponse.data
-        .filter((element) => (element.status == 'Short'))
+        .filter((element) => element.status == 'Short')
         .length.toString();
       this.info[4].value = this.toteDataResponse.data
-        .filter((element) => (element.status == 'Complete'))
+        .filter((element) => element.status == 'Complete')
         .length.toString();
       this.info[1].value = this.toteDataResponse.data
-        .filter((element) => (element.status == 'Missed'))
+        .filter((element) => element.status == 'Missed')
         .length.toString();
-        this.info[5].value = this.toteDataResponse.data
-        .filter((element) => (element.status == 'Not Inducted'))
+      this.info[5].value = this.toteDataResponse.data
+        .filter((element) => element.status == 'Not Inducted')
         .length.toString();
-        this.isBlossomed = this.toteDataResponse.blossomCount > 0;
+      this.isBlossomed = this.toteDataResponse.blossomCount > 0;
     }
+
+
+      let elements: NodeListOf<Element>[] = [];
+    
+      if (this.markoutPreference?.missed === true) {
+        elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.Reprocess-card'));
+      }
+      if (this.markoutPreference?.short === true) {
+        elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.Open-card'));
+      }
+      if (this.markoutPreference?.complete === true) {
+        elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.Compete-cart'));
+      }
+      if (this.markoutPreference?.notIncluded === true) {
+        elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.not-inducted-status'));
+      }
+      if (this.markoutPreference?.shipShort === true) {
+        elements.push(this.el.nativeElement.querySelectorAll('app-info-card-component mat-card.label-blue2'));
+      }
+    
+      // Apply styles to all selected elements
+      elements.forEach(nodeList => {
+        nodeList.forEach(element => {
+          this.renderer.setStyle(element, 'box-shadow', 'inset 0 0 0 4px #1A1AD3');
+        });
+      });
+ 
+    console.log(this.markoutPreference.missed);
+    console.log(this.markoutPreference.short);
+    console.log(this.markoutPreference.shipShort);
   }
 }
