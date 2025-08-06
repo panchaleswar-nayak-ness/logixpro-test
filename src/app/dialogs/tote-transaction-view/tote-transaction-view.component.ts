4e045002940969ec94300def5e36d8e4d686efd0
abdulwahabe64 <abdul.wahab@enterprise64.com>
2025-08-06 17:21:16 +0500

    PLST-6889:[PATCH] LogixPro | Induction | Process Putaways

--- src/app/dialogs/tote-transaction-view/tote-transaction-view.component.ts
+++ src/app/dialogs/tote-transaction-view/tote-transaction-view.component.ts
@@ -142,12 +142,9 @@ export class ToteTransactionViewComponent implements OnInit {
 
             this.dataSource = new MatTableDataSource<any>(res.data);
             this.dataSource.sortingDataAccessor = (item, property) => {
-              console.log('Type of location:', property);
-              console.log('Type of item:', item);
               switch (property) {
 
                 case 'itemLocation':
-                  console.log('Type of location:', typeof item.location);
                   return item.itemLocation?.toString().trim().toLowerCase();
                   
                 default:
