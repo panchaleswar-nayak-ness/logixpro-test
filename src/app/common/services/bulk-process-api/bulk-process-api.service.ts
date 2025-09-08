38d81c91810cde8245582231c5ed5a62ca9a1927
Shan Asghar <99873157+shanasghar-e64@users.noreply.github.com>
2025-09-08 19:41:29 +0500

    PLST-10626:[MAJOR] AHC | LogixPro-v15 | Slapper Label Functionality (#1526)
    Moved API call of update case qty at Tote entry, instead of task complete.

--- src/app/common/services/bulk-process-api/bulk-process-api.service.ts
+++ src/app/common/services/bulk-process-api/bulk-process-api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiFuntions } from '../ApiFuntions';
 import { AuthService } from 'src/app/common/init/auth.service';
 import { IBulkProcessApiService } from './bulk-process-api-interface'
-import {AssignToteToOrderDto, NextToteId, PartialToteIdRequest, PartialToteIdResponse, RemoveOrderLinesRequest, RemoveOrderLinesResponse, TaskCompleteNewRequest} from "../../Model/bulk-transactions";
+import { AssignToteToOrderDto, NextToteId, OrderLineResource, PartialToteIdRequest, PartialToteIdResponse, RemoveOrderLinesRequest, RemoveOrderLinesResponse } from "../../Model/bulk-transactions";
 import { ZoneListPayload } from 'src/app/bulk-process/preferences/preference.models';
 import { ApiResult } from '../../types/CommonTypes';
 
@@ -114,7 +114,7 @@ export class BulkProcessApiService implements IBulkProcessApiService {
     return this.Api.AssignToteToOrder(orders);
   }
   
-  public updateOpenTransactionsZoneCaseQuantity(body: TaskCompleteNewRequest[]) {
+  public updateOpenTransactionsZoneCaseQuantity(body: OrderLineResource[]) {
     return this.Api.updateOpenTransactionsZoneCaseQuantity(body);
   }
   
