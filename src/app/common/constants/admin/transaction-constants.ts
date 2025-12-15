615f2f7734d4d87d577dd7002e9407c05fa28774
Shan Asghar <99873157+shanasghar-e64@users.noreply.github.com>
2025-12-15 09:42:35 +0500

    PLST-11223:[PATCH] LogixPro | Automation | Manual Transactions (#1717)
    * PLST-11223:[PATCH] LogixPro | Automation | Manual Transactions | “Transaction quantity must be greater than zero” conflict does not appear when selecting Save and Post Transaction and Post and Delete Transaction
    
    Refactor transaction validation and centralize messages
    
    Moved transaction quantity and warehouse validation messages to the showNotificationMessage constants for consistency and maintainability. Updated GenerateTransactionComponent to use these centralized messages and improved validation logic for transaction quantity and unit of measure. Removed unused imports and redundant console logs.
    
    * Refactor transaction quantity validation logic
    
    Extracted transaction quantity validation into a dedicated method for reuse and clarity. Introduced setTransactionError helper to reduce code duplication and ensure consistent error handling. Updated related methods to use the new validation approach and initialized transQuantity as a number.
    
    * Refactor transaction notification messages to new constants file
    
    Moved transaction-related notification messages from strings.constants.ts to a new TransactionNotificationMessage object in transaction-constants.ts. Updated GenerateTransactionComponent to use the new constants, improving organization and maintainability of notification messages.

--- src/app/common/constants/admin/transaction-constants.ts
+++ src/app/common/constants/admin/transaction-constants.ts
@@ -5,3 +5,10 @@ export const TransactionConstants = {
     ITEM_NUMBER_FILTER_TEMPLATE: "[Item Number] = '{0}'",
     SHOW_ALL_FILTER: '1=1'
 }
+
+export const TransactionNotificationMessage = {
+    TransactionQuantityMustBePositive: (transactionType: string) => `Transaction Quantity must be a positive integer for transaction type ${transactionType} `,
+    TransactionQuantityMustBeGreaterThanZero: (transactionType: string) => `Transaction Quantity must be greater than zero for transaction type ${transactionType} `,
+    SpecifiedItemNumberMustHaveAWarehouse: 'Specified Item Number must have a Warehouse',
+    UnitOfMeasureDoesNotMatchInventoryMaster: 'Unit of Measure does not match Inventory Master. (Expecting)'
+}
